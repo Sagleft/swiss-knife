5fedc4c8fff85ca38b2c82edf30e3dff6560dba7
Sagleft <sagleft@inbox.ru>
2022-06-29 18:16:08 +0300

    add `ReadFile` func

--- files.go
+++ files.go
@@ -1,12 +1,24 @@
 package swissknife
 
-import "io/ioutil"
+import (
+	"errors"
+	"io/ioutil"
+)
 
 // ReadFileToString read file to string
 func ReadFileToString(filepath string) (string, error) {
 	file, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return "", err
+		return "", errors.New("failed to read file: " + err.Error())
 	}
 	return string(file), err
+}
+
+// ReadFile read file to string
+func ReadFile(filepath string) ([]byte, error) {
+	fileBytes, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, errors.New("failed to read file: " + err.Error())
+	}
+	return fileBytes, err
 }
