b5d3c35dfdc3c0fdb0723191273503bb9bd9b484
Sagleft <sagleft@inbox.ru>
2023-02-19 17:37:27 +0200

    add `PrintIntroMessage` func

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/Sagleft/swiss-knife
 
 go 1.18
 
-require github.com/sagleft/simple-cron v1.4.1
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/fatih/color v1.14.1
+	github.com/sagleft/simple-cron v1.4.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
---
+++ helpers.go
@@ -0,0 +1,26 @@
+package swissknife
+
+import (
+	"fmt"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/fatih/color"
+)
+
+const introMessage = "If you have a bit of extra coin, you can drop the developer a coffee.\n" +
+	"So that he gets drunk on coffee and happily runs to develop this and other projects"
+const previewColor = "green"
+
+// PrintIntroMessage - publishes a funny message in the log asking to donate to the developer
+func PrintIntroMessage(appName, cryptoAddress string, cryptoCoinTag ...string) {
+	coinTag := ""
+	if len(cryptoCoinTag) > 0 {
+		coinTag = cryptoCoinTag[0]
+	}
+
+	figure.NewColorFigure(appName, "", previewColor, true).Print()
+	fmt.Println()
+	fmt.Println(introMessage)
+	color.Green(cryptoAddress + " " + coinTag)
+	fmt.Println()
+}
