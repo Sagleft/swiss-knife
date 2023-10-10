1c44e93096065e8c58add9811c43132b0148ccfc
Sagleft <sagleft@inbox.ru>
2023-10-10 22:36:32 +0300

    add `SaveStructToJSONFileIndent` func

--- structs.go
+++ structs.go
@@ -13,3 +13,13 @@ func SaveStructToJSONFile(i interface{}, filepath string) error {
 
 	return SaveStringToFile(filepath, string(jsonr))
 }
+
+// SaveStructToJSONFileIndent save arbitrary structure to JSON file
+func SaveStructToJSONFileIndent(i interface{}, filepath string) error {
+	jsonr, err := json.MarshalIndent(i, "", "	")
+	if err != nil {
+		return err
+	}
+
+	return SaveStringToFile(filepath, string(jsonr))
+}
